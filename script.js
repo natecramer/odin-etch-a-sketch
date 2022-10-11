3509aede14f6e52d96ed0522483db5f25e52604d
Nate Cramer <natecramer@gmail.com>
2022-10-10 22:45:01 -0400

    Add different draw styles

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function clamp(n, min, max) {
+    return Math.min(Math.max(n, min), max);
+}
+
 let theGrid = document.querySelector('#theGrid');
 
 let w_in_cells = 16;
@@ -18,7 +22,8 @@ function generateGrid(size) {
         let row = i / w_in_cells;
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.addEventListener('mouseover', () => cell.classList.add('cellHover'));
+        cell.addEventListener('mouseover', fillCell);
+        cell.style['background-color'] = 'darkslategray'
 
         theGrid.appendChild(cell);
     }    
@@ -43,5 +48,50 @@ gridButton.addEventListener('click', () => {
         generateGrid(size);
     }
 });
+
+// select draw style
+let drawStyleSelect = document.querySelector('#drawStyle');
+let drawStyle = 'yellow';
+function setDrawStyle() {
+    drawStyle = drawStyleSelect.value;
+}
+
+function fillCell() {
+    switch (drawStyle) {
+        case 'yellow':
+            this.style['background-color'] = 'yellow';
+            break;
+        case 'random':
+            this.style['background-color'] = `rgb(
+                ${Math.random() * 255},
+                ${Math.random() * 255},
+                ${Math.random() * 255}
+            )`;
+            break;
+        case 'fade':
+            bgColor = '';
+            bgColor = window.getComputedStyle(this, null).getPropertyValue('background-color');
+            
+            // extract rgb from bgColor
+            const re = /.*?\(([0-9]*?), ([0-9]*?), ([0-9]*?)\)/;
+            rgba = bgColor.match(re);
+
+            if (!rgba)
+                return;
+
+            r = rgba[1];
+            g = rgba[2];
+            b = rgba[3];
+
+            // fade to black
+            r = clamp(r - 25.5, 0, 255);
+            g = clamp(g - 25.5, 0, 255);
+            b = clamp(b - 25.5, 0, 255);
+
+            this.style['background-color'] = `rgb(${r}, ${g}, ${b})`;
+            
+            break;
+    }
+}
 
 generateGrid(16);
